fddbfe3fafb05782f0cb41b7f77b538676bc0a9d
Paskuvan <majopaskuvan@gmail.com>
2021-11-24 20:59:54 -0300

    update files

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React, {useState,} from 'react';
+import React, { useState } from 'react';
 
 import AddUser from './components/Users/AddUser';
-import UsersList from './components/Users/UserList';
-
+import UsersList from './components/Users/UsersList';
 
 function App() {
   const [usersList, setUsersList] = useState([]);
 
   const addUserHandler = (uName, uAge) => {
-    setUsersList((prevUserList) => {
-      return [...prevUserList, {name: uName, age: uAge}];
-      });
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
   };
+
   return (
     <div>
       <AddUser onAddUser={addUserHandler} />
--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import Card from '../UI/Card';
-import Button from '../UI/Button';
+
+import Card from './Card';
+import Button from './Button';
 import classes from './ErrorModal.module.css';
 
 const ErrorModal = (props) => {
-    return (
-    <div className={classes.backdrop}>
-    <Card className={classes.modal}>
+  return (
+    <div>
+      <div className={classes.backdrop} onClick={props.onConfirm} />
+      <Card className={classes.modal}>
         <header className={classes.header}>
-            <h2>{props.title}</h2>
+          <h2>{props.title}</h2>
         </header>
         <div className={classes.content}>
-            <p>{props.message}</p>
+          <p>{props.message}</p>
         </div>
         <footer className={classes.actions}>
-          <Button>Okay</Button>
+          <Button onClick={props.onConfirm}>Okay</Button>
         </footer>
-    </Card>
+      </Card>
     </div>
-    );
+  );
 };
 
 export default ErrorModal;
